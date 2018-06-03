efe54629f76a7766cd4bb3e0bcc18b0f918043d2
TheJoas <joao_curado_sousa@hotmail.com>
2018-06-03 22:13:40 +0100

    PopUp Added

--- app/src/main/java/com/joas/worten/wortenleasuretimeapp/SecondFragment.java
+++ app/src/main/java/com/joas/worten/wortenleasuretimeapp/SecondFragment.java
@@ -1,13 +1,26 @@
 package com.joas.worten.wortenleasuretimeapp;
 
 import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 /**
  * Created by Joas on 26-Mar-18.
@@ -32,8 +45,7 @@ public class SecondFragment extends Fragment {
 
     // Inflate the view for the fragment based on layout XML
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_second, container, false);
 
         btnRestaurant = view.findViewById(R.id.Restaurant);
@@ -49,7 +61,32 @@ public class SecondFragment extends Fragment {
                         break;
                     }
                     case MotionEvent.ACTION_UP:
+                        showPopup(v);
+                        /*
                         // Your action here on button click
+                        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+                        String parameters= "location=-33.8670522,151.1957362&radius=500&type=restaurant"+ "&key=" + BuildConfig.Places;
+                        url = url + parameters;
+                        //get image https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyBpc55OOKepU6Gzd2A2seEnc_0RUr0rfyc
+                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        Log.i("Resposta",response.toString());
+                                    }
+                                }, new Response.ErrorListener() {
+
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        // TODO: Handle error
+
+                                    }
+                                });
+
+                        // Access the RequestQueue through your singleton class.
+                        Volley.newRequestQueue(getContext()).add(jsonObjectRequest);*/
+
                     case MotionEvent.ACTION_CANCEL: {
                         Button view = (Button) v;
                         view.getBackground().clearColorFilter();
@@ -63,6 +100,28 @@ public class SecondFragment extends Fragment {
 
         return view;
     }
+
+    public void showPopup(View anchorView) {
+
+        View popupView = getActivity().getLayoutInflater().inflate(R.layout.popup, null);
+
+        PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        // If the PopupWindow should be focusable
+        popupWindow.setFocusable(true);
+
+        // If you need the PopupWindow to dismiss when when touched outside
+        popupWindow.setBackgroundDrawable(new ColorDrawable());
+        int location[] = new int[2];
+
+        // Get the View's(the one that was clicked in the Fragment) location
+        anchorView.getLocationOnScreen(location);
+
+        // Using location, the PopupWindow will be displayed right under anchorView
+        popupWindow.showAtLocation(anchorView, Gravity.CENTER, 0, 0);
+
+    }
+
 }
 
 
